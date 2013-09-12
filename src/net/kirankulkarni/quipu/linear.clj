993deb8dbf2a1c622ed93ff332aa8d86526fa109
Kiran Kulkarni <kk.questworld@gmail.com>
2013-09-12 17:37:15 +0530

    Add function to calculate bit-size given true cardinality
    i.e. given n calculate m

--- src/net/kirankulkarni/quipu/linear.clj
+++ src/net/kirankulkarni/quipu/linear.clj
@@ -24,7 +24,7 @@
 ;;;          or set it to max cardinality you expect.
 ;;; m      - Size of Bit Array that will be used
 
-(deftype LinearCounting
+(deftype LinearCounter
     [^:unsynchronized-mutable ^java.util.BitSet bitset n m]
   IProbCountingSet
   (add [this elem]
@@ -52,3 +52,78 @@
         vn (Math/log (/ un m))
         n-estimated (* (- m) vn)]
     (long (Math/round n-estimated))))
+
+
+(def ^:private predefind-map-sizes
+  "Maps n to m"
+  {100 5034
+   200 5067
+   300 5100
+   400 5133
+   500 5166
+   600 5199
+   700 5231
+   800 5264
+   900 5296
+   1000 5329
+   2000 5647
+   3000 5957
+   4000 6260
+   5000 6556
+   6000 6847
+   7000 7132
+   8000 7412
+   9000 7688
+   10000 7960
+   20000 10506
+   30000 12839
+   40000 15036
+   50000 17134
+   60000 19156
+   70000 21117
+   80000 23029
+   90000 24897
+   100000 26729
+   200000 43710
+   300000 59264
+   400000 73999
+   500000 88175
+   600000 101932
+   700000 115359
+   800000 128514
+   900000 141441
+   1000000 154171
+   2000000 274328
+   3000000 386798
+   4000000 494794
+   5000000 599692
+   6000000 702246
+   7000000 802931
+   8000000 902069
+   9000000 999894
+   10000000 1096582
+   50000000 4584297
+   100000000 8571013
+   120000000 10112529})
+
+(def ^:private predefind-n
+  (sort (keys predefind-map-sizes)))
+
+(def ^:private predefind-n-intervals
+  (mapv (fn [x0 x1]
+          [x0 x1])
+        predefind-n
+        (rest predefind-n)))
+
+
+(defn calculate-m
+  "Get Number of bits required for given n"
+  [n]
+  (assert (<= n (last predefind-n)))
+  (or (predefind-map-sizes n)
+      (let [[x0 x1] (first (filter #(and (> n (first %))
+                                         (< n (second %)))
+                                   predefind-n-intervals))
+            y0 (get predefind-map-sizes x0)
+            y1 (get predefind-map-sizes x1)]
+        (qu/lerp x0 y0 x1 y1 n))))
