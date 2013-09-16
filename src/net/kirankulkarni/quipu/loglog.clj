2b509b7a433556e50919a2bd62c286a0fadc5d78
Kiran Kulkarni <kk.questworld@gmail.com>
2013-09-17 03:08:27 +0530

    Add function to add-element in LogLog Counter

--- src/net/kirankulkarni/quipu/loglog.clj
+++ src/net/kirankulkarni/quipu/loglog.clj
@@ -1,14 +1,34 @@
 (ns ^{:doc "LogLog Implementation"
       :author "Kiran Kulkarni <kk@helpshift.com>"}
   net.kirankulkarni.quipu.loglog
-  )
+  (:require [net.kirankulkarni.quipu.protocols
+             :refer [to-byte-array]]
+            [net.kirankulkarni.quipu.core
+             :refer :all]
+            [net.kirankulkarni.quipu.utils :as qu]))
 
 
 (defn square [x] (* x x))
 
 (defn log2 [x] (/ (Math/log x) (Math/log 2)))
 
-(defn pow-2 [x] (Math/pow 2 x))
+(defn pow2 [x] (Math/pow 2 x))
+
+(defn >>> [v bits] (bit-shift-right (bit-and 0xFFFFFFFF v) bits))
+
+
+(defn- add-elem
+  [bytearray k elem]
+  (let [elem-hash (qu/murmurhash-32 elem)
+        j (>>> elem-hash (- Integer/SIZE k))
+        r (+ 1
+             (Integer/numberOfLeadingZeros
+              (bit-or (bit-shift-left elem-hash k)
+                      (bit-shift-left 1 (dec k)))))]
+    (when (< (aget bytearray j) r)
+      (aset-byte bytearray j r)
+      true)))
+
 
 (defn find-closest
   [x coll]
@@ -27,5 +47,6 @@
     (let [m-given (square (/ 1.30 expected-error))
           m (find-closest m-given m-in-pow2)
           k (int (log2 m))
-          max-n (* m (pow-2 (pow-2 8)))]
-      (assert (<= n max-n)))))
+          max-n (* m (pow2 (pow2 8)))]
+      (assert (<= k 24) "K should be smaller than 24")
+      (assert (<= n max-n) (str "Can not support given cardinality " n)))))
