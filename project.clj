1c558a648e39664024425d0e9419b264706c3a8f
Kiran Kulkarni <kk.questworld@gmail.com>
2013-10-13 20:08:14 +0530

    Update project.clj with URL and descriptions

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject net.kirankulkarni/quipu "0.1.0"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Implements a bunch of Probabilistic data structures to count cardinality of a set"
+  :url "https://github.com/kirankulkarni/quipu"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
